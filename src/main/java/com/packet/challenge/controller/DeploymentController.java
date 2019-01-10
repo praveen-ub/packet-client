74ae8f68b17e707f82125158f17ab566adafab0d
Praveen kumar Rajendran <praveenkumarrajendran@Praveens-MBP-2.fios-router.home>
2019-01-09 20:40:05 -0500

    Integrated create device

--- src/main/java/com/packet/challenge/controller/DeploymentController.java
+++ src/main/java/com/packet/challenge/controller/DeploymentController.java
@@ -1,10 +1,14 @@
 package com.packet.challenge.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.packet.challenge.service.DeviceService;
 import com.packet.challenge.service.ProjectService;
 
 @RestController
@@ -14,11 +18,53 @@ public class DeploymentController{
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private DeviceService deviceService;
+	
 	
 	@RequestMapping("/project_events")
 	public JsonNode getProjectEvents(){
 		
 		return projectService.getEvents();
 	}
+	
+	public boolean startUserAcceptanceTesting(boolean retainEnvironment) throws IOException{
+		
+		/*
+		 * Plan - t1x small, OS: Ubuntu 16.04 LTS, facility: Amsterdam | Any
+		 */
+		
+		String sampleDeviceConfig = "{\"plan\": \"e69c0169-4726-46ea-98f1-939c9e8a3607\", "
+									+ "\"operating_system\": \"1b9b78e3-de68-466e-ba00-f2123e89c112\""
+									+ "\"facility\":[\"8e6470b3-b75e-47d1-bb93-45b225750975\", \"any\"]}"
+									+ "\"tags\":[\"jan9-staging\"]";
+		
+		//Externalise this to JacksonUtils
+		ObjectMapper mapper = new ObjectMapper();
+	    JsonNode deviceConfig = mapper.readTree(sampleDeviceConfig);
+	    deviceService.createDevice(deviceConfig);
+	  
+//	    while(true){
+//	    		
+//	    		//keep polling for device status
+//	    }
+		
+		//create device with sample config
+		//poll device status to see if its ready
+		//if ready, deploy code and start the app servers
+		//collect device information like IP
+		//once server is started, trigger acceptance testing using the device's ip
+		//Once acceptance testing is done, mail the report
+		//Tear down the environment is retainEnvironment is false
+		//else power off the server and return the device details for futher access
+		
+		//Handle provisioning error
+		//Handle de-provisioning error
+		
+		return true;
+		
+	}
+	
+	
 	
 }
--- src/main/java/com/packet/challenge/service/DeviceService.java
+++ src/main/java/com/packet/challenge/service/DeviceService.java
@@ -1,17 +1,29 @@
 package com.packet.challenge.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.packet.challenge.config.PacketConfig;
 
 @Service
 public class DeviceService{
 	
-//	public JsonNode createDevice(String deviceId){
-//		
-//	}
-//	
-//	
-//	public boolean deleteDevice(String deviceId){
-//		
-//		
-//	}
+	public JsonNode createDevice(JsonNode deviceConfig){
+		
+		RestTemplate restTemplate = new RestTemplate();
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+		headers.add("X-Auth-Token", PacketConfig.AUTH_TOKEN);
+		headers.add("Content-Type", "application/json");
+		HttpEntity<JsonNode> httpEntity = new HttpEntity<JsonNode> (deviceConfig,headers);
+		String url = PacketConfig.PROJECTS_ENDPOINT+"/"+PacketConfig.PROJECT_ID+"/devices";
+		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, JsonNode.class);
+		return response.getBody();	
+	}
+
 }
