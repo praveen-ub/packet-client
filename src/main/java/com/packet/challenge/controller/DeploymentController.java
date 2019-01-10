6a032532443efa40a1d6e1e682334bff6c71b1b4
Praveen kumar Rajendran <praveenkumarrajendran@Praveens-MBP-2.fios-router.home>
2019-01-09 21:48:14 -0500

    Integrated Delete Device, Device Details and Device actions

--- src/main/java/com/packet/challenge/controller/DeploymentController.java
+++ src/main/java/com/packet/challenge/controller/DeploymentController.java
@@ -28,43 +28,55 @@ public class DeploymentController{
 		return projectService.getEvents();
 	}
 	
-	public boolean startUserAcceptanceTesting(boolean retainEnvironment) throws IOException{
+	@RequestMapping("trigger_acceptance_testing")
+	public boolean runUserAcceptanceTesting(boolean retainEnvironment) throws IOException, InterruptedException{
 		
-		/*
+		/* Sample config
 		 * Plan - t1x small, OS: Ubuntu 16.04 LTS, facility: Amsterdam | Any
 		 */
-		
+			
+		//These config should ideally be obtained as an input
 		String sampleDeviceConfig = "{\"plan\": \"e69c0169-4726-46ea-98f1-939c9e8a3607\", "
 									+ "\"operating_system\": \"1b9b78e3-de68-466e-ba00-f2123e89c112\""
 									+ "\"facility\":[\"8e6470b3-b75e-47d1-bb93-45b225750975\", \"any\"]}"
 									+ "\"tags\":[\"jan9-staging\"]";
 		
+		
+		
 		//Externalise this to JacksonUtils
 		ObjectMapper mapper = new ObjectMapper();
 	    JsonNode deviceConfig = mapper.readTree(sampleDeviceConfig);
-	    deviceService.createDevice(deviceConfig);
+	    JsonNode deviceDetails = deviceService.createDevice(deviceConfig); //create device could fail
+	    String deviceId = deviceDetails.get("id").asText();
+	    //log device information
 	  
-//	    while(true){
-//	    		
-//	    		//keep polling for device status
-//	    }
-		
+	    while(true){ //Ideally, this should timeout after a cerain waiting time
+	    	
+	    	if("active".equalsIgnoreCase(deviceService.getDeviceStatus(deviceId))){
+	    		break;
+	    	}
+	    	Thread.sleep(60*1000);
+	    }
+		//retrieve IP address
 		//create device with sample config
 		//poll device status to see if its ready
-		//if ready, deploy code and start the app servers
-		//collect device information like IP
-		//once server is started, trigger acceptance testing using the device's ip
+	    
+	    //Device is available at this point
+		//deploy code and start the app servers
+		//once server is started, trigger acceptance testing using the device's ip   
 		//Once acceptance testing is done, mail the report
+	    
+	    //Pausing for time taken for the app deployment & testing scripts to complete  
+	    Thread.sleep(5*60*1000);
+	    
 		//Tear down the environment is retainEnvironment is false
-		//else power off the server and return the device details for futher access
-		
-		//Handle provisioning error
-		//Handle de-provisioning error
-		
+		//else power off the server
+	    if(!retainEnvironment){
+	    	deviceService.deleteDevice(deviceId);
+	    }	
+	    else{
+	    	deviceService.performAction(deviceId, "power_off");
+	    }
 		return true;
-		
 	}
-	
-	
-	
 }
--- src/main/java/com/packet/challenge/service/DeviceService.java
+++ src/main/java/com/packet/challenge/service/DeviceService.java
@@ -1,6 +1,7 @@
 package com.packet.challenge.service;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,48 @@ public class DeviceService{
 		String url = PacketConfig.PROJECTS_ENDPOINT+"/"+PacketConfig.PROJECT_ID+"/devices";
 		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity, JsonNode.class);
 		return response.getBody();	
+	}
+	
+	public JsonNode getDeviceDetails(String deviceId){
+
+		//handle device not found
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("X-Auth-Token", PacketConfig.AUTH_TOKEN); //Move string literals to constants
+		HttpEntity<String> httpEntity = new HttpEntity<String> ("headers",headers);
+		String url = PacketConfig.DEVICES_ENDPOINT+"/"+deviceId+"/events";
+		ResponseEntity<JsonNode> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, JsonNode.class);
+		return response.getBody();
+	}
+
+	public String getDeviceStatus(String deviceId){
+
+		//handle device not found
+		JsonNode deviceDetails = getDeviceDetails(deviceId);
+		return deviceDetails.get(0).get("state").asText();
+	}
+	
+	public boolean performAction(String deviceId, String action){
+		
+		RestTemplate restTemplate = new RestTemplate();
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+		headers.add("X-Auth-Token", PacketConfig.AUTH_TOKEN);
+		HttpEntity<Object> request = new HttpEntity<Object>(headers);
+		String url = PacketConfig.DEVICES_ENDPOINT+"/"+deviceId+"/actions?type="+action;
+		restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+		return true;
+	}
+	
+	
+	public boolean deleteDevice(String deviceId){
+		
+		RestTemplate restTemplate = new RestTemplate();
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
+		headers.add("X-Auth-Token", PacketConfig.AUTH_TOKEN);
+		HttpEntity<Object> request = new HttpEntity<Object>(headers);
+		String url = PacketConfig.DEVICES_ENDPOINT+"/"+deviceId;
+		restTemplate.exchange(url, HttpMethod.DELETE, request, String.class);
+		return true;
 	}
 
 }
